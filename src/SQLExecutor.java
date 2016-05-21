7d91dd1a674f684ac405a4ea88ad5d1fefb50eac
Peter van der Wal <peterzakelijk@live.nl>
2016-05-21 16:25:06 +0200

    1000 inserts bitches
    ss

--- src/SQLExecutor.java
+++ src/SQLExecutor.java
@@ -8,12 +8,9 @@ import java.sql.SQLException;
  */
 public class SQLExecutor {
     public static final String INSERT_CUSTOMERS = "INSERT INTO BK.KLANT "
-            + "(VOORNAAM, ACHTERNAAM, GESLACHT, MOBIEL_NUMMER, HUISNUMMER, POSTCODE, PLAATS, START_DATUM_CONTRACT, EIND_DATUM_CONTRACT, ABONNEMENT_NAAM) "
-            + "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-    public static final String INSERT_CUSTOMERS_TEST = "INSERT INTO BK.KLANT "
-            + "(VOORNAAM, ACHTERNAAM, GESLACHT, STRAATNAAM, HUISNUMMER, POSTCODE, "
-            + "MOBIEL_NUMMER, PLAATS, START_DATUM_CONTRACT, EIND_DATUM_CONTRACT, ABONNEMENT_NAAM) "
-            + "VALUES( 'Peter', 'van, 'm' , ?, ?, ?, ?, ?, ?) ;";
+            + "(VOORNAAM, ACHTERNAAM, GESLACHT, MOBIEL_NUMMER, HUISNUMMER, POSTCODE, PLAATS, START_DATUM_CONTRACT, EIND_DATUM_CONTRACT, ABONNEMENT_NAAM, STRAATNAAM) "
+            + "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
 
     private Connector conn;
     public SQLExecutor(Connector conn){
@@ -21,52 +18,48 @@ public class SQLExecutor {
     }
 
     public void insertCustomers(){
+        int phoneNumber = 16670000;
+        int customerID = 2;
+        int housenumber = 1;
         conn.connect();
         PreparedStatement prep = null;
-        try {
-            prep = conn.getConnection().prepareStatement(INSERT_CUSTOMERS);
-            prep.setString(1, "Henk");
-            prep.setString(2, "deVries");
-            prep.setString(3, String.valueOf("m"));
-            prep.setString(4, "05134528917");
-            prep.setInt(5, 442);
-            prep.setString(6, "9741MC");
-            prep.setString(7, "Groningen");
-            prep.setDate(8,  Date.valueOf("2016-04-21"));
-            prep.setDate(9, Date.valueOf("2016-03-21"));
-            prep.setString(10, "SMS_BUDGET");
+        for(int i=0; i<1000; i++) {
+            try {
+                prep = conn.getConnection().prepareStatement(INSERT_CUSTOMERS);
+                prep.setString(1, "Klant-" + customerID);
+                prep.setString(2, "achternaam");
+                prep.setString(3, String.valueOf("m"));
+                prep.setString(4, "06-" + phoneNumber);
+                prep.setInt(5, housenumber);
+                prep.setString(6, "9741MC");
+                prep.setString(7, "Groningen");
+                prep.setDate(8, Date.valueOf("2016-04-21"));
+                prep.setDate(9, Date.valueOf("2016-03-21"));
+                prep.setString(10, "SMS_BUDGET");
+                prep.setString(11, "Klantenstraat");
 
-//            prep.setString(1, "TestKlant");
-//            prep.setString(2, "Achternaam");
-//            //prep.setString(3, String.valueOf('m'));
-//            prep.setString(3, "TestStraat");
-//            prep.setInt(4, 1);
-//            prep.setString(5, "9741MC");
-//            prep.setString(6, "0616675928");
-//            prep.setString(7, "Groningen");
-//           // prep.setDate(8, Date.valueOf("2016-04-21"));
-//           // prep.setDate(9, Date.valueOf("2016-05-21"));
-//            prep.setString(8, "SMS_BUDGET");
-//            System.out.println(prep.toString());
-            ResultSet rs = prep.executeQuery();
-            String executed_query = rs.getStatement().toString();
-            System.out.println("Test: " + executed_query);
+                ResultSet rs = prep.executeQuery();
+                String executed_query = rs.getStatement().toString();
+                System.out.println("Test: " + executed_query);
+                phoneNumber++;
+                customerID++;
+                housenumber++;
 
 
-        }catch(SQLException e){
-            e.printStackTrace();
-        }
-        finally {
-            if(prep != null){
-                try {
-                    prep.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } finally {
+                if (prep != null) {
+                    try {
+                        prep.close();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
                 }
-            }
-            conn.closeConnection();
-        }
 
+            }
+        }
+        conn.closeConnection();
     }
 
 }

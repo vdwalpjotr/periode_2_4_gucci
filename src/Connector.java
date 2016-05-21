e00782ed73ee5371e09de6029e8d8b4c39d3be67
Peter van der Wal <peterzakelijk@live.nl>
2016-05-21 13:00:16 +0200

    werkende connectie

---
+++ src/Connector.java
@@ -0,0 +1,30 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Connector class connects to the oracle database
+ */
+public class Connector {
+    public static final String DRIVER_URL = "oracle.jdbc.OracleDriver";
+    public static final String DATABASE_URL = "jdbc:oracle:thin:system/oracle@192.168.99.100:49161:xe";
+    private Connection conn;
+    public void connect(){
+        try {
+            Class.forName(DRIVER_URL);
+        } catch (ClassNotFoundException NFE) {
+            NFE.printStackTrace();
+        }
+        try {
+            conn = DriverManager.getConnection(DATABASE_URL);
+            if (conn != null) {
+                System.out.println("Connected");
+            }
+        } catch (SQLException SQLE) {
+            SQLE.printStackTrace();
+        }finally{
+            //TODO close connection
+        }
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * Created by peter on 21-May-16.
  */
 public class Main {
+
+    public static void main(String[] args) {
+        Connector connector = new Connector();
+        connector.connect();
+    }
 }

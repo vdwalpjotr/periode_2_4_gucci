f7ef2e10efd4e8ea673362ad2a1ae329d39f28be
timzijl <timzijl@hotmail.com>
2016-05-22 11:55:59 +0200

    3balmost

--- src/SQLExecutor.java
+++ src/SQLExecutor.java
@@ -1,7 +1,4 @@
-import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 /**
  * Created by peter on 21-May-16.
@@ -59,6 +56,68 @@ public class SQLExecutor {
 
             }
         }
+        conn.closeConnection();
+    }
+
+    public void select_query(int customer_id, String month) {
+        conn.connect();
+
+        try {
+            PreparedStatement get_id = null;
+            String query1 = "SELECT KLANT.MOBIEL_NUMMER " +
+                            "FROM BK.KLANT " +
+                            "WHERE KLANT.KLANT_ID = ?";
+
+            get_id = conn.getConnection().prepareStatement(query1);
+            get_id.setInt(1,customer_id);
+            ResultSet klant_set = get_id.executeQuery();
+            klant_set.next();
+
+            String phone_number = klant_set.getString("MOBIEL_NUMMER");
+            System.out.println(phone_number);
+
+            PreparedStatement count_sms_customer = null;
+            String query2 = "SELECT COUNT(*) AS count_sms " +
+                            "FROM BK.SMS_HISTORIE " +
+                            "WHERE SMS_HISTORIE.MOBIEL_ZENDER = ?";
+
+            count_sms_customer = conn.getConnection().prepareStatement(query2);
+            count_sms_customer.setString(1,phone_number);
+            ResultSet sms_set = count_sms_customer.executeQuery();
+            sms_set.next();
+
+            int count_sms = sms_set.getInt("count_sms");
+            conn.getConnection().
+
+            PreparedStatement count_bel_customer = null;
+            String query3 = "SELECT " +
+                    "  SUM( " +
+                    "    ((EXTRACT(DAY FROM (BEL_HISTORIE.EIND_DATUM_TIJD - BEL_HISTORIE.START_DATUM_TIJD)) * (24 * 60)) + " +
+                    "    (EXTRACT(HOUR FROM (BEL_HISTORIE.EIND_DATUM_TIJD - BEL_HISTORIE.START_DATUM_TIJD)) * 60) + " +
+                    "    EXTRACT(MINUTE FROM (BEL_HISTORIE.EIND_DATUM_TIJD - BEL_HISTORIE.START_DATUM_TIJD))) " +
+                    "  ) AS sum_minutes " +
+                    "FROM BK.BEL_HISTORIE " +
+                    "WHERE BEL_HISTORIE.MOBIEL_ZENDER = ? " +
+                    "AND to_char(BEL_HISTORIE.START_DATUM_TIJD, 'Month') LIKE ? " +
+                    "AND to_char(BEL_HISTORIE.EIND_DATUM_TIJD, 'Month') LIKE ?";
+
+            count_bel_customer = conn.getConnection().prepareStatement(query3);
+            count_bel_customer.setString(1, phone_number);
+            count_bel_customer.setString(2, "%" + month + "%");
+            count_bel_customer.setString(3, "%" + month + "%");
+            ResultSet bel_set = count_bel_customer.executeQuery();
+            bel_set.next();
+
+            int count_bel = bel_set.getInt("sum_minutes");
+
+            System.out.println("Klant: " + customer_id + " Maand: " + month);
+            System.out.println("Aantal SMS: " + count_sms);
+            System.out.println("Aantal Belminuten: " + count_bel);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
         conn.closeConnection();
     }
 
